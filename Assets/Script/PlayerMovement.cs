7ac2125dd4bb3be38b2b005583c294ba0da3b3fa
gabbbb457 <gabrielamellizo45@gmail.com>
2026-08-02 01:13:53 -0500

    qsueño

--- Assets/Script/PlayerMovement.cs
+++ Assets/Script/PlayerMovement.cs
@@ -1,27 +1,41 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
-
 {
     public float speed = 5f;
-    public float jumpForce = 10f;
+    public float jumpForce = 8f;
 
     private Rigidbody2D rb;
+    private Animator animator;
+    private SpriteRenderer spriteRenderer;
+
     private bool isGrounded;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     void Update()
     {
+        // Movimiento horizontal
         float move = Input.GetAxisRaw("Horizontal");
 
         rb.velocity = new Vector2(move * speed, rb.velocity.y);
 
+        // Girar personaje sin cambiar el tamaño
+        if (move > 0)
+            spriteRenderer.flipX = false;
+        else if (move < 0)
+            spriteRenderer.flipX = true;
+
+        // Parámetros del Animator
+        animator.SetFloat("Speed", Mathf.Abs(move));
+        animator.SetBool("IsGrounded", isGrounded);
+
+        // Salto
         if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
         {
             rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
