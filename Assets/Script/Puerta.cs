67248f0c48872c87be84933c76e5f4cea7c9309f
ccdfmck <maria_paula.zambrano@uao.edu.co>
2026-08-01 20:04:27 -0500

    AV5
    Cuando llega a la puerta ya cambia d escena

--- Assets/Script/Puerta.cs
+++ Assets/Script/Puerta.cs
@@ -1,19 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Puerta : MonoBehaviour
 
 {
-    public GameObject pantallaVictoria;
-
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
             if (GameManager.instancia.tieneLlave)
             {
-                pantallaVictoria.SetActive(true);
+                SceneManager.LoadScene("Victoria");
             }
             else
             {
